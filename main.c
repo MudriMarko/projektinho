5ae4b45846f9586c73d5bfcc46f4c2c467e6ffbe
MudriMarko <marko.mariic10@gmail.com>
2025-06-05 21:54:03 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,75 +1,82 @@
 ﻿#include "cvijece.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-int main() {
-	Cvijet* skladiste = NULL;
-	Prodaja* prodaje = NULL;
-	int brojCvjetova = 0, kapacitet = 0;
-	int brojProdaja = 0, kapacitetProdaja = 10;
+void main_menu(void) {
+    int choice = 0;
+    do {
+        printf("\n--- Glavni izbornik ---\n");
+        printf("1. Dodaj cvijet\n");
+        printf("2. Prikaz cvjetova\n");
+        printf("3. Ažuriraj cvijet\n");
+        printf("4. Obrisi cvijet\n");
+        printf("5. Sortiraj cvjetove\n");
+        printf("6. Pretraži cvijet\n");
+        printf("7. Kupi cvijeće\n");
+        printf("8. Operacije na datotekama\n");
+        printf("9. Izlaz\n");
+        printf("Odaberite opciju: ");
 
-	ucitajSkladisteIzdatoteke(&skladiste, &brojCvjetova, &kapacitet);
+        if (scanf("%d", &choice) != 1) {
+            printf("Neispravan unos.\n");
+            clear_input_buffer();
+            continue;
+        }
+        clear_input_buffer();
 
-	prodaje = malloc(kapacitetProdaja * sizeof(Prodaja));
-	if (!prodaje) {
-		ERROR_MSG("Neuspjelo alociranje memorije za prodaje!");
-		oslobodiMemoriju(&skladiste, NULL);
-		return 1;
-	}
+        switch (choice) {
+        case MAIN_MENU_ADD:
+            add_flower();
+            break;
+        case MAIN_MENU_LIST:
+            list_flowers();
+            break;
+        case MAIN_MENU_UPDATE:
+            update_flower();
+            break;
+        case MAIN_MENU_DELETE:
+            delete_flower();
+            break;
+        case MAIN_MENU_SORT:
+            sort_flowers();
+            break;
+        case MAIN_MENU_SEARCH:
+            search_flowers();
+            break;
+        case MAIN_MENU_BUY:
+            buy_flowers();
+            break;
+        case MAIN_MENU_FILE_OPS:
+            file_ops_menu();
+            break;
+        case MAIN_MENU_EXIT:
+            printf("Spremanje podataka...\n");
+            if (save_flowers() == 0) {
+                printf("Podaci su spremljeni.\n");
+            }
+            else {
+                printf("Greška pri spremanju podataka!\n");
+            }
+            safe_free((void**)&flower_list);
+            flower_count = 0;
+            printf("Izlaz iz programa.\n");
+            break;
+        default:
+            printf("Nevažeći odabir. Pokušajte ponovno.\n");
+            break;
+        }
+    } while (choice != MAIN_MENU_EXIT);
+}
 
-	Izbornik izbor;
-	do {
-		printf("\n--- Izbornik ---\n");
-		printf("1. Dodaj cvijet\n");
-		printf("2. Prikazi skladiste\n");
-		printf("3. Prodaja cvijeca\n");
-		printf("4. Azuriraj cvijet\n");
-		printf("5. Obrisi cvijet\n");
-		printf("6. Spremi skladiste\n");
-		printf("7. Sortiraj skladiste\n");
-		printf("0. Izlaz\n");
-		printf("Unesite izbor: ");
-		int tempIzbor;
-		if (scanf("%d", &tempIzbor) != 1) {
-			printf("Neispravan unos!\n");
-			while (getchar() != '\n');
-			continue;
-		}
-		izbor = (Izbornik)tempIzbor;
+int main(void) {
+    if (load_flowers() != 0) {
+        printf("Došlo je do problema pri učitavanju baze cvjetova.\n");
+        flower_list = NULL;
+        flower_count = 0;
+    }
+    else {
+        printf("Baza cvjetova učitana. Broj cvjetova: %d\n", flower_count);
+    }
 
-		switch (izbor) {
-		case IZBOR_DODAJ:
-			dodajCvijet(&skladiste, &brojCvjetova, &kapacitet);
-			break;
-		case IZBOR_ISPISI:
-			ispisiSkladiste(skladiste, brojCvjetova);
-			break;
-		case IZBOR_PRODAJA:
-			prodajaCvijeca(skladiste, brojCvjetova, &prodaje, &brojProdaja, &kapacitetProdaja);
-			break;
-		case IZBOR_AZURIRAJ:
-			azurirajCvijet(skladiste, brojCvjetova);
-			break;
-		case IZBOR_BRISE:
-			obrisiCvijet(&skladiste, &brojCvjetova);
-			break;
-		case IZBOR_SPREMI:
-			spremiSkladisteUdatoteku(skladiste, brojCvjetova);
-			break;
-		case IZBOR_SORTIRAJ:
-			qsort(skladiste, brojCvjetova, sizeof(Cvijet), usporediCvijet);
-			printf("Skladiste je sortirano po nazivu.\n");
-			break;
-		case IZBOR_IZLAZ:
-			printf("Izlaz iz programa.\n");
-			break;
-		default:
-			printf("Nepoznat izbor, probajte ponovo.\n");
-			break;
-		}
+    main_menu();
 
-	} while (izbor != IZBOR_IZLAZ);
-
-	oslobodiMemoriju(&skladiste, &prodaje);
-	return 0;
+    return 0;
 }
